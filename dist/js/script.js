be49a54870afb50e3b4d88c7d8f277af21bc28a5
Jesús <js.less@hotmail.com>
2020-01-24 12:33:48 -0600

    Cards responsive y uso de swiper.js para la creación de los caroussels

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,20 +1,70 @@
 /* 
-
     window.getComputedStyle(elemento).getPropertyValue //lee los estilos de un elemento
     element.style//todo elemento del DOM tiene la propiedad style para definir estilos
-
-
 */
 // document.body.style.backgroundColor = 'red'
 
 // Para leer, necesitamos el elemento
 // console.log(getComputedStyle.getPropertyPriorityValue(--color__brand))
 
-addEventListener('resize', () => {
-    const mobileBp = matchMedia('(min-width: 800px)')
-    if (mobileBp.matches) {
-        document.body.style.backgroundColor = 'red'
-    } else {
-        document.body.style.backgroundColor = 'yellow'
+
+// const styles = `
+//   background: yellow;
+//   border:4px solid red;  
+// `
+// document.getElementById('mobile_card').setAttribute('style', styles)
+// addEventListener('resize', () => {
+//     const mobileBp = matchMedia('(min-width: 800px)')
+//     if (mobileBp.matches) {
+//         document.body.style.backgroundColor = 'red'
+//     } else {
+//         document.body.style.backgroundColor = 'yellow'
+//     }
+// })
+
+// const mobileBp = matchMedia('(min-width: 800px)')
+
+// const changeSizeCard = mql => {
+//     if(mql.matches){
+//         const styles = `
+//             background: green;
+//             border:4px solid red;  
+//             display:inline-flex;
+//             flex-direction: column;
+//             z-index: 99999;
+//             position: relative;
+//         `
+//         document.getElementById('mobile_card').setAttribute('style', styles) 
+//     }
+//     else{
+//         const styles = `
+//             background: yellow;
+//             border:4px solid red;  
+//             display:inline-flex;
+//             flex-direction: column;
+//             z-index: 99999;
+//             position: relative;
+//         `
+//         document.getElementById('mobile_card').setAttribute('style', styles)
+//         // document.body.style.background = 'yellow'
+//     } 
+// }
+// changeSizeCard(mobileBp)
+// mobileBp.addEventListener(changeSizeCard)
+
+//Manejarlo por clases por CSS
+
+
+const swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 900,
+      modifier: 1,
+      slideShadows : false,
     }
-})
+  });
