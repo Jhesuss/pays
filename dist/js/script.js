15bb01becc25293b8d1ecbdb181f054d43a100ea
Jesús <js.less@hotmail.com>
2020-02-04 22:33:05 -0600

    Este commit es para guardar los cambios por un problema que tengo del equipo que tengo que reiniciar para poder continuar trabajando.
    Hasta este momento he acomodado la vista de desktop, solo hace falta
    acomodar que no haga swipe a los lados y listo.

--- dist/js/script.js
+++ dist/js/script.js
@@ -61,23 +61,32 @@ const swiper = new Swiper('.swiper-container', {
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-      rotate: 2,
-      stretch: 0,
-      depth: 900,
+      rotate: 1,
+      stretch: 60,
+      depth: 500,
       modifier: 1,
       slideShadows : false,
     }, 
     pagination: {
        el: '.swiper-pagination',
-    },    
+       clickable: 'true',
+    },
+
   });
 
+  
   
   document.querySelector('.slide-1').addEventListener('click', function (e) {
     e.preventDefault();
     swiper.slideTo(0, 0);
+
   });
   document.querySelector('.slide-250').addEventListener('click', function (e) {
     e.preventDefault();
     swiper.slideTo(249, 0);
   });
+
+  if($(".slider .slide").length == 1) {
+    $('.swiper-wrapper').addClass( "disabled" );
+    $('.swiper-pagination').addClass( "disabled" );
+}
